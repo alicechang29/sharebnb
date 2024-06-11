c4a4ac4cfe52b77022c7b4a9fa64fce0b00dcac0
Andrea Jao <ndrjao@gmail.com>
2024-06-11 16:53:50 -0700

    added download

--- aws-tutorial.py
+++ aws-tutorial.py
@@ -29,20 +29,21 @@ for bucket in response['Buckets']:
     print(f'  {bucket["Name"]}')
 
 
-# bucket: s3 bucket name
-# key: s3 file path (eg: test/123/a.jpg)
-# file_path: local file path
-# def upload_img(bucket: S3_BUCKET, key: str, file_path: str):
-#     s3.upload_file(Bucket=bucket, Key=key, Filename=file_path, ExtraArgs={
-#         'ContentType': "image/jpeg"})
-#     os.remove(file_path)
-#     print("upload image {} success".format(file_path))
+def upload_content(key: str, origin_file_path: str):
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=app.config['aws_access_key_id'],
+                        aws_secret_access_key=app.config['aws_secret_access_key'])
+    s3.Bucket(S3_BUCKET).upload_file(origin_file_path, key)
 
-# print("!!!!!!", S3_BUCKET.Bucket(S3_BUCKET))
-s3 = boto3.resource('s3',
-                    aws_access_key_id=app.config['aws_access_key_id'],
-                    aws_secret_access_key=app.config['aws_secret_access_key'])
 
-print("!!!!!s3", s3)
-s3.Bucket(S3_BUCKET).upload_file(
-    '/Users/alicechang/rithm/sharebnb/potato_salad.jpeg', 'potato_salad2.jpeg')
+# NOTE: be careful to not put a "/" in front of an object key if don't want folder
+# upload_content('potato_salad3.jpeg', 'potato_salad.jpeg')
+
+
+def download_content(key: str, destination_file_path: str):
+    s3 = boto3.resource('s3', aws_access_key_id=app.config['aws_access_key_id'],
+                        aws_secret_access_key=app.config['aws_secret_access_key'])
+    s3.Bucket(S3_BUCKET).download_file(key, destination_file_path)
+
+
+download_content('/potato_salad.jpeg', '/')
