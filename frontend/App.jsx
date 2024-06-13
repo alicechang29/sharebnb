94ba01836062300ce3e85abc7faf1f79faab5d88
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-13 10:55:54 -0700

    added todos

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import { sendImageToServer } from "./api/src/api/api";
+import { sendFormDataToServer } from "./api/src/api/api";
 
 /** Sharebnb Application
  * FIXME:
@@ -11,18 +11,21 @@ function App() {
   console.debug("App");
 
   /**Handling submission of image */
-  async function handleImgSubmit(evt) {
+  async function handleFormSubmit(evt) {
     evt.preventDefault();
+    console.log("!!!!!", evt.target.multifiles.files);
     //FIXME: this is all data, including other form data
-    const imageData = new FormData();
-    imageData.append('image', evt.target.image.files[0]);
-    imageData.append('dummy', "hello");
+    const formData = new FormData();
+    formData.append('image', evt.target.image.files[0]);
+    //TODO: to handle multiple images, need to append each individually to formData. THIS IS NOT AN ARRAY, FILELIST IS ITS OWN OBJECT
+    // formData.append('multi-images', evt.target.multifiles.files);
+    formData.append('text-test', "hello");
 
-    console.log("file data", imageData);
+    console.log("file data", formData);
 
     // TODO: call API with this data
     try {
-      await sendImageToServer(imageData);
+      await sendFormDataToServer(formData);
     } catch (err) {
       console.log(err);
     }
@@ -31,9 +34,10 @@ function App() {
   return (
     <div className="App">
       App Placeholder Input Form
-      <form onSubmit={handleImgSubmit}>
+      <form onSubmit={handleFormSubmit}>
         <input type="file" name="image"></input>
-        <input type="text" name="dummy"></input>
+        <input type="file" id="files" name="multifiles" multiple></input>
+        <input type="text" name="text-test"></input>
         <button type="submit">Click Me</button>
       </form>
     </div>
