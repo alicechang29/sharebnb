f2964dc411018c48f1becffec51f277713343cf3
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-13 12:17:28 -0700

    notes

--- frontend/api/src/api/api.js
+++ frontend/api/src/api/api.js
@@ -2,6 +2,10 @@
 /** Sends image data to Flask server*/
 async function sendFormDataToServer(formData) {
   console.log("add data server", formData);
+
+  //FIXME:  do a try - return an ok message
+  //catch - return the err and have the front end handle the error
+
   const resp = await fetch('http://localhost:5001/api/add-listing', {
     method: "POST",
     header: {
