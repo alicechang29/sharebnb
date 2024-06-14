ab4ba73aefb01611c4c9afdfed7c9175522a04e1
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-14 13:48:42 -0700

    removed palceholder

--- frontend/src/AddListingForm.jsx
+++ frontend/src/AddListingForm.jsx
@@ -98,7 +98,7 @@ function AddListingForm({ addListing }) {
                   id="title"
                   autoComplete="title"
                   className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="janesmith123"
+                  placeholder="title"
                 />
               </div>
             </div>
