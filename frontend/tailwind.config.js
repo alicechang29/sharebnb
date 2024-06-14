099e30d5dcee8e93cdc97dc1b438a60c0087c583
Andrea Jao <ndrjao@gmail.com>
2024-06-14 10:17:51 -0700

    made the form styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
+
 }
 
