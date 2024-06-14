4458ee0f3a7bcd58cabb81c4f6ad27402d1c93ec
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-14 11:20:29 -0700

    fixed listings component

--- frontend/src/Listings.jsx
+++ frontend/src/Listings.jsx
@@ -17,30 +17,31 @@ function Listings({ listings }) {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {listings.listings.map((listing) => (
-            <div key={listing.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={listing.images[0]}
-                  alt={listing.images[0]}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <Link className="listing" to={`/${listing.id}`} key={listing.id}>
-                      {listing.title}
-                    </Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{listing.description}</p>
+            <Link className="listing" to={`${listing.id}`} key={listing.id}>
+              <div key={listing.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+
+                  <img
+                    src={listing.images[0]}
+                    alt={listing.images[0]}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
                 </div>
-                <p className="text-sm font-medium text-gray-900">{listing.price}</p>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      {listing.title}
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{listing.description}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{listing.price}</p>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
-    </div>
+    </div >
 
   );
 }
