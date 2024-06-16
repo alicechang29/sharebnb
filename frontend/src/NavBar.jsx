6395ad25c0edeabf5c8f4beb07479c9c9d6d0ea1
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-15 19:32:19 -0700

    fixed NavBar

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,101 +1,30 @@
 import { Link, NavLink } from "react-router-dom";
 import { Nav, NavItem } from "reactstrap";
 import React from "react";
-import { Fragment } from "react";
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-/** Top navigation bar for site. */
+import "./NavBar.css";
 
 function NavBar() {
   console.log("* NavBar");
 
-  const navigation = [
-    { name: "Home", href: "/", current: true },
-    { name: "Listings", href: "/listings", current: false },
-    { name: "New Listing", href: "/add-listing", current: false },
-  ];
-
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-  }
-
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Link
-                  className="navbar-brand text-white rounded-md px-3 py-2 text-lg font-medium"
-                  to="/"
-                >
-                  ShareBnB
-                </Link>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto rounded-full"
-                    src="https://i.imgur.com/QO85cVY.png"
-                    alt="Your Company"
-                  />
-                </div>
-                <Nav className="hidden sm:ml-6 sm:block">
-                  <NavItem className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <NavLink
-                        key={item.name}
-                        to={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </NavLink>
-                    ))}
-                  </NavItem>
-                </Nav>
-              </div>
-            </div>
-          </div>
+    <div>
+      <nav className="NavBar navbar navbar-expand-md">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            ShareBnB
+          </Link>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink to="/listings">Listings</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/add-listing">Add New</NavLink>
+            </NavItem>
+          </Nav>
+        </div>
+      </nav>
 
-          <DisclosurePanel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-          </DisclosurePanel>
-        </>
-      )}
-    </Disclosure>
+    </div>
   );
 }
 
